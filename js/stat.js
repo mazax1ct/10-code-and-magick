9861b3a147e2e2cf987ed2ddf0dc6c715db50a0a
mazax1ct <vova@giveandget.ru>
2018-04-09 16:15:39 +0300

    Fix. Откат package.json и package-lock.json, рефакторинг кода

--- js/stat.js
+++ js/stat.js
@@ -4,40 +4,39 @@
 window.renderStatistics = function (ctx, names, times) {
 
   // вводим переменные для отрисовки облака
-  var cloudXDefault = 100; // позиция "облака" по горизонтали
-  var cloudYDefault = 10; // позиция "облака" по вериткали
-  var cloudWidthDefault = 420; // ширина "облака"
-  var cloudHeightDefault = 270; // высота "облака"
-  var cloudColorDefault = 'rgb(255,255,255)'; // цвет "облака"
-  var cloudShadowDefault = true; // наличие тени
+  var CLOUD_X = 100; // позиция "облака" по горизонтали
+  var CLOUD_Y = 10; // позиция "облака" по вериткали
+  var CLOUD_WIDTH = 420; // ширина "облака"
+  var CLOUD_HEIGHT = 270; // высота "облака"
+  var CLOUD_COLOR = 'rgb(255,255,255)'; // цвет "облака"
 
-  var fontDefault = '16px PT Mono'; // размер и гарнитура шрифта
-  var fontColorDefault = 'Black'; // цвет шрифта
+  var FONT = '16px PT Mono'; // размер и гарнитура шрифта
+  var FONT_COLOR = 'Black'; // цвет шрифта
 
   // вводим переменные для отрисовки столбцов
-  var columnMaxHeightDefault = 150; // высота гистограммы (максимальная высота столбца)
-  var graphicOffsetYDefault = 100; // позиция диаграммы по Y
-  var columnStartDefault = 140; // позиция первого столбца по горизонтали
-  var columnWidthDefault = 40; // ширина столбца
-  var columnOffsetXDefault = 50; // отступ между столбцами
+  var COLUMN_MAXHEIGHT = 150; // высота гистограммы (максимальная высота столбца)
+  var GRAPHIC_OFFSET_Y = 100; // позиция диаграммы по Y
+  var COLUMN_START_X = 140; // позиция первого столбца по горизонтали
+  var COLUMN_WIDTH = 40; // ширина столбца
+  var COLUMN_OFFSET_X = 50; // отступ между столбцами
   var alpha = function () {
     return 1 - 0.7 * Math.random(); // варируем прозрачность для изменения насыщенности цвета колонки в разумных пределах чтобы не получить прозрачный столбец
   };
-  var currentColumnColorDefault = 'rgba(255, 0, 0, 1)'; // цвет столбца для игрока "Вы"
+  var COLUMN_COLOR = 'rgba(255, 0, 0, 1)'; // цвет столбца для игрока "Вы"
 
   // функция отрисовки "облака" с тенью
-  var cloudDraw = function (cloudX, cloudY, cloudWidth, cloudHeight, cloudColor, cloudShadow, font, fontColor) {
-    if (cloudShadow === true) {
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-      ctx.fillRect(cloudX + 10, cloudY + 10, cloudWidth, cloudHeight);
-    }
+  var cloudDraw = function () {
+    // рисуем тень
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(CLOUD_X + 10, CLOUD_Y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-    ctx.fillStyle = cloudColor;
-    ctx.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
+    // рисуем "облако"
+    ctx.fillStyle = CLOUD_COLOR;
+    ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
     // выводим текст
-    ctx.font = font;
-    ctx.fillStyle = fontColor;
+    ctx.font = FONT;
+    ctx.fillStyle = FONT_COLOR;
     ctx.fillText('Ура вы победили!', 120, 40);
     ctx.fillText('Список результатов:', 120, 60);
 
@@ -59,8 +58,8 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   // функция отрисовки столбцов
-  var columnDraw = function (maxTime, columnMaxHeight, graphicOffsetY, columnStart, columnWidth, columnOffsetX, currentColumnColor, font, fontColor) {
-    var startX = columnStart; // обозначаем начальную точку по X для отрисовки столбца, у первого он равен 140
+  var columnDraw = function (maxTime) {
+    var startX = COLUMN_START_X; // обозначаем начальную точку по X для отрисовки столбца, у первого он равен 140
     var startY = 0; // обозначаем начальную точку по Y для отрисовки столбца
 
     // переменные для высоты столбца
@@ -70,37 +69,37 @@ window.renderStatistics = function (ctx, names, times) {
     // перебираем массив имен и рисуем колонки
     for (var i = 0; i < names.length; i++) {
       // если не первый столбец то рисуем его от края с отступом в 140 + ширина + отступ
-      startX = columnStart + columnWidth * i + columnOffsetX * i;
+      startX = COLUMN_START_X + COLUMN_WIDTH * i + COLUMN_OFFSET_X * i;
 
       // считаем высоту столбца
       height = (times[i] / maxTime) * 100; // высота в процентах
-      heightPx = (columnMaxHeight * height) / 100; // высота в px
+      heightPx = (COLUMN_MAXHEIGHT * height) / 100; // высота в px
 
       // считаем позицию столбца по Y
-      startY = graphicOffsetY + (columnMaxHeight - heightPx);
+      startY = GRAPHIC_OFFSET_Y + (COLUMN_MAXHEIGHT - heightPx);
 
       if (names[i] === 'Вы') { // меняем цвет столбца если текущий игрок "Вы"
-        ctx.fillStyle = currentColumnColor;
+        ctx.fillStyle = COLUMN_COLOR;
       } else {
         ctx.fillStyle = 'rgba(0, 0, 255,' + alpha() + ')';
       }
-      ctx.fillRect(startX, startY, columnWidth, heightPx);
+      ctx.fillRect(startX, startY, COLUMN_WIDTH, heightPx);
 
       // рисуем имена под столбцами
-      ctx.font = font;
-      ctx.fillStyle = fontColor;
-      ctx.fillText(names[i], startX, graphicOffsetY + columnMaxHeight + 20);
+      ctx.font = FONT;
+      ctx.fillStyle = FONT_COLOR;
+      ctx.fillText(names[i], startX, GRAPHIC_OFFSET_Y + COLUMN_MAXHEIGHT + 20);
 
       // рисуем результаты времени столбцами
-      ctx.font = font;
-      ctx.fillStyle = fontColor;
+      ctx.font = FONT;
+      ctx.fillStyle = FONT_COLOR;
       ctx.fillText(Math.round(times[i]), startX, startY - 10);
     }
   };
 
   // вызов функции отрисовки "облака"
-  cloudDraw(cloudXDefault, cloudYDefault, cloudWidthDefault, cloudHeightDefault, cloudColorDefault, cloudShadowDefault, fontDefault, fontColorDefault);
+  cloudDraw();
 
   // вызов функции отрисовки столбцов
-  columnDraw(getMaxTime(times), columnMaxHeightDefault, graphicOffsetYDefault, columnStartDefault, columnWidthDefault, columnOffsetXDefault, currentColumnColorDefault, fontDefault, fontColorDefault);
+  columnDraw(getMaxTime(times));
 };
